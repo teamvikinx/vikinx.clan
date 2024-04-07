1241a87e8b67e9263258c010235795e7bdd0afd5
Saad Shaikh <shksaad911@gmail.com>
2024-04-07 21:28:41 +0530

    added story page and minor enhancements

---
+++ app/api/review/route.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server";
+import { HttpStatusCode } from "axios";
+import admin from "@/lib/config/firebase";
+import { constants } from "@/lib/utils";
+
+const db = admin.firestore();
+
+export async function POST(req: NextRequest) {
+  try {
+    const payload = (await req.json()) as IReview;
+    const _docRef = await db
+      .collection(constants.tables.review)
+      .where("review_by.id", "==", payload.review_by.id)
+      .get();
+    const alreadyExist = _docRef.docs.map((x) => x.data());
+    console.log(alreadyExist.length > 0);
+
+    if (alreadyExist.length > 0) {
+      return NextResponse.json(
+        { message: "You already have submitted you story with us!" },
+        { status: 200 }
+      );
+    }
+
+    const docRef = db.collection(constants.tables.review).doc(payload.uuid);
+
+    await docRef.set(payload);
+
+    return NextResponse.json(
+      { message: "Thank you so much for sharing your story with us! 🎉" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: error },
+      { status: HttpStatusCode.BadRequest }
+    );
+  }
+}
---
+++ lib/validations/review.validation.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+
+export const reviewValidation = z.object({
+  comment: z
+    .string()
+    .min(30, { message: "Minimum 30 characters required" })
+    .max(1000, { message: "Reached max capacity of 1000 characters" }),
+});
